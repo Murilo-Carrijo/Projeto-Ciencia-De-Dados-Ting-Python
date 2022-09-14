f6f57434551bf91cc994eb1bcdd5aff638a2204e
murilo <murilocarrijoadm@gmail.com>
2022-09-14 16:47:06 -0300

    feat: req 3 - implementacao da funcao process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    """Pre-processando o arquivo """
+    file = txt_importer(path_file)
+
+    if path_file not in instance._data:
+        instance.enqueue(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    return sys.stdout.write(str(result))
 
 
 def remove(instance):
